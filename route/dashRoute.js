1caaafc8cced5269dc9ae0e9d76cf5ce35d4eec9
unknown <jonathanbuntuguh@yahoo.com>
2025-03-26 14:17:36 +0000

    26/3/2025 b

--- route/dashRoute.js
+++ route/dashRoute.js
@@ -2,7 +2,6 @@ const { authenticate } = require('../controller/authController');
 const { getDash } = require('../controller/dashController');
 
 const router = require('express').Router();
-
 router.route('/').get(authenticate, getDash);
 
 module.exports = router;
